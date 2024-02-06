1fece0f46beb011eab2cb53502355874461c87ef
Udeme98 <Darealudeme1@gmail.com>
2024-02-06 05:28:41 +0100

    updated App.jsx, Tailwind.config and index.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div>
+      <h1>Hello world!</h1>
+      <h1 className="text-[40px]">Hello world!</h1>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        vuejs: "#49e659",
+      },
+    },
   },
   plugins: [],
 };
