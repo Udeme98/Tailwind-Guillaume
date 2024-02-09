d5d10dc887ae954f95d7eba87b257221c424248a
Udeme98 <Darealudeme1@gmail.com>
2024-02-09 20:29:10 +0100

    updated Anitrans.jsx

--- src/Concepts/Anitrans.jsx
+++ src/Concepts/Anitrans.jsx
@@ -1,7 +1,14 @@
-export default function Effect() {
+export default function Anitrans() {
   return (
-    <div>
-      <h2>Animations and Transition</h2>
+    <div className="p-4">
+      <div className="inline-block p-4 text-black bg-white border rounded-lg shadow-md">
+        <h2 className="text-2xl">Transition</h2>
+        <p className="mb-4">This is my div</p>
+        {/* transition and animation*/}
+        <button className="px-3 py-2 text-white rounded-lg cursor-pointer bg-cyan-500 shadow-sm shadow-cyan-500/50 active:shadow-none -translate-y-1 transition ease-in-out delay animate hover:scale-105">
+          Say hello
+        </button>
+      </div>
     </div>
   );
 }
