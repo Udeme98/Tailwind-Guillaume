077e994ddeaeb0bbb9f8bdd1ab4fe2d6de8cb313
Udeme98 <Darealudeme1@gmail.com>
2024-02-10 11:09:21 +0100

    updated Design.jsx

--- src/Concepts/Design.jsx
+++ src/Concepts/Design.jsx
@@ -5,15 +5,42 @@ export default function Design() {
       <h2>This is a subtitle</h2>
       <p>This is a paragraph</p>
       <a href="#">This is an anchor</a>
+
+      {/* styling buttons */}
+      <div className="my-4">
+        <button className="btn btn-primary">This is a primary button</button>
+      </div>
+      <div className="my-4">
+        <button className="btn btn-secondary">
+          This is a secondary Button
+        </button>
+      </div>
+      <div className="my-4">
+        <button disabled className="btn btn-primary">
+          This is a primary button
+        </button>
+      </div>
+
+      {/* stying input elements */}
       <div>
-        <button>This is a button</button>
+        <input type="text" placeholder="Name" />
       </div>
       <div>
-        <input type="text" />
+        <input disabled type="text" placeholder="Disabled" />
       </div>
       <div>
+        <input type="date" placeholder="Disabled" />
+      </div>
+
+      {/* checkbox */}
+      <div className="flex items-start">
         <input type="checkbox" id="checkbox" />
-        <label htmlFor="checkbox">Checkbox</label>
+        <label htmlFor="checkbox">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta magni
+          doloremque quisquam doloribus repudiandae libero perferendis vel ea
+          maxime quidem eum, rem iusto eaque eius ipsum commodi neque ratione
+          alias!
+        </label>
       </div>
       <div>
         <select name="option" id="option">
@@ -21,8 +48,6 @@ export default function Design() {
           <option value="2">Option 2</option>
           <option value="3">Option 3</option>
         </select>
-
-        <h1>Helo</h1>
       </div>
     </div>
   );
